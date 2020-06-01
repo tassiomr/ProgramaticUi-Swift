ab4fc54febbdbfe0955733c9be22c53f5562798e
Tassio Rocha <=>
2020-06-01 10:10:05 -0300

    Remove border and add background color

--- ProgramaticUi-Swift/Components/StyledTextField.swift
+++ ProgramaticUi-Swift/Components/StyledTextField.swift
@@ -24,15 +24,13 @@ class StyledTextField: UITextField {
 		super.init(coder: coder);
 	}
 	
-	
 	func setupTextField(placeholder: String, isSecureTextEntry: Bool = false) {
 		self.heightAnchor.constraint(equalToConstant: TextFiledContants.height).isActive = true;
 		self.clipsToBounds = true
+		self.backgroundColor = .white
 		self.layer.cornerRadius = TextFiledContants.radius;
 		self.leftView = PaddingView()
 		self.leftViewMode = .always
-		self.layer.borderWidth = 1
-		self.layer.borderColor = UIColor.lightGray.cgColor
 		self.placeholder = placeholder
 		self.isSecureTextEntry = isSecureTextEntry
 	}
