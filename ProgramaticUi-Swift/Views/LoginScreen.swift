b942a49eb535a2f7e6f505325ae3d06a32433067
Tassio Rocha <=>
2020-06-01 10:00:42 -0300

    Refactor login screen to use .pin function

--- ProgramaticUi-Swift/Views/LoginScreen.swift
+++ ProgramaticUi-Swift/Views/LoginScreen.swift
@@ -19,22 +19,17 @@ class LoginScreen: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.setupProgrammaticUI()
-		self.setupContratinNextButton()
-		self.setupConstraintTextField()
 		self.view.backgroundColor = .white;
 		navigationController?.setNavigationBarHidden(true, animated: false)
 		// Do any additional setup after loading the view.
 	}
 	
 	func setupProgrammaticUI() {
-		button.setupButton(bgColor: .orange, target: self, action: #selector(goToSecondPage), title: "Next");
-		textField.setupTextField(placeholder: "Username");
-		passwordTextField.setupTextField(placeholder: "Password", isSecureTextEntry: true);
-		
 		view.addSubview(textField);
 		view.addSubview(passwordTextField);
 		view.addSubview(button);
 		
+		setupContrainst()
 	}
 	
 	@objc func goToSecondPage() {
@@ -42,20 +37,14 @@ class LoginScreen: UIViewController {
 		navigationController?.pushViewController(homeScreen, animated: true);
 	}
 	
-	func setupContratinNextButton () {
-		button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-		button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-		button.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 162).isActive = true;
-	}
-	
-	func setupConstraintTextField() {
-		textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-		textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-		textField.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+	func setupContrainst() {
+		button.setupButton(bgColor: .orange, target: self, action: #selector(goToSecondPage), title: "Next");
+		textField.setupTextField(placeholder: "Username");
+		passwordTextField.setupTextField(placeholder: "Password", isSecureTextEntry: true);
 		
-		passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
-		passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
-		passwordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 66).isActive = true
+		textField.pin(view: view);
+		passwordTextField.pin(view: view, yAnchor: 66)
+		button.pin(view: view, yAnchor: 162)
 	}
 
 }
