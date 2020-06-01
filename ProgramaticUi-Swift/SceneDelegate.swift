9313f1ed326fab740f5e82f466b2778e139dadcb
Tassio Rocha <=>
2020-06-01 09:51:57 -0300

    Update scene delegate to call new screens

--- ProgramaticUi-Swift/SceneDelegate.swift
+++ ProgramaticUi-Swift/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 	
 		guard let scene = (scene as? UIWindowScene) else { return }
 		
-		let firstScreen = FirstScreen()
+		let firstScreen = LoginScreen()
 		let navigationCOntroller = UINavigationController(rootViewController: firstScreen);
 		
 		
